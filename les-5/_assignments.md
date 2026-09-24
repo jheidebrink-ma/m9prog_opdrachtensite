49133f5d11a22cf10ab705e6334ace5257ae51ba
Jasper - MA <j.heidebrink@ma-web.nl>
2026-09-24 10:15:13 +0200

    webpack

--- les-5/_assignments.md
+++ les-5/_assignments.md
@@ -7,4 +7,23 @@
 4. Enqueue alleen de gecompilede bestanden vanuit je theme.
 5. Controleer met een productie-build dat de website zonder ontwikkelserver goed werkt.
 
+## Voorbeeld
+In dit voorbeeld heb ik in de thema folder een `src` folder met daarin een `js`, `scss`, `images` en `fonts` folder. 
+Door gebruik te maken van mijn webpack kopiëer en compile ik de bestanden naar de `dist` folder. Deze `dist` fodler gaat straks naar de live omgeving, mijn `src` folder blijft alleen **lokaal** en in **git**. 
+### Fonts
+In de fonts folder zitten fonts die ik kopieer naar de dist folder en die ik in mijn scss gebruik. 
+### Images
+In de images folder zitten afbeeldingen die ik kopieer naar de dist folder en die ik in thema gebruik.
+### Scss
+In de scss folder zit een `main.scss` bestand waarin ik bootstrap importeer en mijn eigen scss bestanden. In de `main.scss` importeer ik eerst mijn variabelen.
+### Js
+In de js folder zit een `main.js` bestand waarin ik mijn eigen js code schrijf en compile naar de dist folder.
+
+### Compile
+De compile process zorgt ervoor dat mijn JavaScript en Sass bestanden worden omgezet naar een vorm die in de browser goed werkt. 
+Dit voer ik straks uit met je `npm run build` command.
+
+### bestanden
+Hier kun je een voorbeeld van de [package.json](./data/package.json) en [webpack.config.js](./data/webpack.config.js) vinden.
+
 **Resultaat:** je front-end wordt reproduceerbaar gecompileerd en heeft een eigen visuele basis.
---
+++ les-5/data/webpack.config.js
@@ -0,0 +1,128 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const outputPath = path.resolve(__dirname, 'dist');
+const bootstrapSvgWarningPattern = /postcss-svgo: .*SvgoParserError: <input>:1:1: Non-whitespace before first tag\./;
+
+const sharedRules = [
+    {
+        test: /\.s?css$/i,
+        use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+                loader: 'sass-loader',
+                options: {
+                    sassOptions: {
+                        quietDeps: true,
+                        silenceDeprecations: ['import', 'global-builtin'],
+                    },
+                },
+            },
+        ],
+    },
+    {
+        test: /\.m?js$/i,
+        exclude: /node_modules/,
+        type: 'javascript/auto',
+    },
+];
+
+module.exports = (env, argv) => {
+    const isDev = argv.mode === 'development';
+
+    const rawConfig = {
+        name: 'raw',
+        mode: isDev ? 'development' : 'none',
+        devtool: false,
+        entry: {
+            'js/main':   './src/js/main.js',
+            'js/admin':  './src/js/admin.js',
+            'css/main':  './src/scss/main.scss',
+            'css/admin': './src/scss/admin.scss',
+        },
+        output: {
+            path: outputPath,
+            filename: '[name].js',
+        },
+        module: { rules: sharedRules },
+        plugins: [
+            new RemoveEmptyScriptsPlugin(),
+            new MiniCssExtractPlugin({ filename: '[name].css' }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: 'src/images',
+                        to: 'images',
+                        noErrorOnMissing: true,
+                        globOptions: {
+                            ignore: ['**/*.zip', '**/*.psd', '**/*.ai'],
+                        },
+                    },
+                    {
+                        from: 'src/fonts',
+                        to: 'fonts',
+                        noErrorOnMissing: true,
+                        globOptions: {
+                            ignore: ['**/*.zip', '**/*.psd', '**/*.ai'],
+                        },
+                    },
+                ],
+            }),
+        ],
+        optimization: { minimize: false },
+    };
+
+    const minifiedConfig = {
+        name: 'minified',
+        mode: 'production',
+        devtool: false,
+        entry: {
+            'js/main.min':   './src/js/main.js',
+            'css/main.min':  './src/scss/main.scss',
+        },
+        output: {
+            path: outputPath,
+            filename: '[name].js',
+        },
+        module: { rules: sharedRules },
+        plugins: [
+            new RemoveEmptyScriptsPlugin(),
+            new MiniCssExtractPlugin({ filename: '[name].css' }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: 'src/images',
+                        to: 'images',
+                        noErrorOnMissing: true,
+                        globOptions: {
+                            ignore: ['**/*.zip', '**/*.psd', '**/*.ai'],
+                        },
+                    },
+                    {
+                        from: 'src/fonts',
+                        to: 'fonts',
+                        noErrorOnMissing: true,
+                        globOptions: {
+                            ignore: ['**/*.zip', '**/*.psd', '**/*.ai'],
+                        },
+                    },
+                ],
+            }),
+        ],
+        optimization: {
+            minimize: true,
+            minimizer: [
+                '...',
+                new CssMinimizerPlugin({
+                    warningsFilter: (warning) => !bootstrapSvgWarningPattern.test(String(warning)),
+                }),
+            ],
+        },
+    };
+
+    return [rawConfig, minifiedConfig];
+};
